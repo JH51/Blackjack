d8422cb1fa2d28c5cd154a7b6e14be61224c9d37
Giorgio Vidali <gvidali@stuy.edu>
2017-01-04 18:37:57 -0500

    Beginning Of GUI Work: for future notice, green is ugly, place buttons on right

---
+++ BlackjackGUI.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+
+
+public class BlackjackGUI extends JFrame{
+
+  private Container pane;
+
+  public BlackjackGUI() {
+    this.setTitle("Vim City Blackjack Casino");
+    this.setSize(750, 500);
+    this.setLocation(100,100);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+  pane = this.getContentPane();
+  pane.setBackground(Color.green);
+  pane.setLayout(new FlowLayout());
+  JButton b = new JButton("Hit");
+  JButton b1 = new JButton("Stay");
+
+  pane.add(b);
+  pane.add(b1);
+
+}
+  public static void main(String[] args) {
+    BlackjackGUI tester = new BlackjackGUI();
+    tester.setVisible(true);
+  }
+
+}
