95f62668514ada2f49f5d4b632cc83cc40b85c33
Giorgio Vidali <gvidali@stuy.edu>
2017-01-06 03:01:41 -0500

    Look into this more, Main things: look into layout and adding images

--- BlackjackGUI.java
+++ BlackjackGUI.java
@@ -21,11 +21,13 @@ public class BlackjackGUI extends JFrame{
   pane.setLayout(new FlowLayout());
   JButton b = new JButton("Hit");
   JButton b1 = new JButton("Stay");
-  JButton b2 = new JButton("Stay");
+  JButton b2 = new JButton("Next Hand");
+
 
   pane.add(b);
   pane.add(b1);
   pane.add(b2);
+
 
 }
   public static void main(String[] args) {
