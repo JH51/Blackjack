b9be74a7aa9412249d60026e1cc06d55f93893fb
Giorgio Vidali <gvidali@stuy.edu>
2017-01-08 14:08:56 -0500

    Blackjack Rules

--- Blackjack.java
+++ Blackjack.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Blackjack {
 
     private Agent player;
@@ -16,23 +18,45 @@ public class Blackjack {
 
     public static void main(String[] args) {
 	Blackjack game = new Blackjack();
-	Deck d = new Deck();
+
+  Deck d = new Deck();
 	d.shuffle();
+  Scanner playerInput = new Scanner(System.in);
 
-//while(player.getMoney() > 0){
 
+  while(game.player.getMoney() > 0){
+
+System.out.println("Current funds" + game.player.getMoney() + "how much to bet?");
+
+int bet = playerInput.nextInt();
+if(bet > game.player.getMoney()){
+  System.out.println("can't bet more than what you have");
+  break;
+}
+//player gets two cards
   game.player.draw(d);
 	game.player.draw(d);
   System.out.println("Your cards are:");
   System.out.println(game.player.getHand()[0].toString());
 	System.out.println(game.player.getHand()[1].toString());
-	game.dealer.draw(d);
+//dealer gets two cards
+  game.dealer.draw(d);
   game.dealer.draw(d);
   System.out.println("The dealer shows");
   System.out.println(game.dealer.getHand()[0].toString());
 
+//loop for action
+  while(true){
+    if(game.dealer.getHand()[0].getValue() + game.dealer.getHand()[1].getValue() == 21){
+      System.out.println("Dealer has Blackjack");
+      game.player.setMoney(game.player.getMoney() - bet);
+    }
+    System.out.println(game.dealer.getHand()[0].getValue() + game.dealer.getHand()[1].getValue());
+    System.out.println(game.player.getHand()[0].getValue() + game.player.getHand()[1].getValue() + " Is your current hand value " );
 
-//}
+   break;
+  }
+ }
     }
 
 }
