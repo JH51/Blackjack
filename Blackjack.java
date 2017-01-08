7eeb99126cecbbd479286a237f47947c6dd0cd36
Giorgio Vidali <gvidali@stuy.edu>
2017-01-08 15:09:39 -0500

    Finished player, going to dealer actions

--- Blackjack.java
+++ Blackjack.java
@@ -54,8 +54,45 @@ if(bet > game.player.getMoney()){
     System.out.println(game.dealer.getHand()[0].getValue() + game.dealer.getHand()[1].getValue());
     System.out.println(game.player.getHand()[0].getValue() + game.player.getHand()[1].getValue() + " Is your current hand value " );
 
+
+    System.out.println("hit(0) to stay and (1) to hit");
+    int hitOrStay = playerInput.nextInt();
+
+//Staying
+    if(hitOrStay == 0){
+      System.out.println(game.player.getHand()[0].getValue() + game.player.getHand()[1].getValue()
+      + " remains your hand value");
+    }
+
+//HITTING
+    if(hitOrStay == 1){
+      game.player.draw(d);
+      System.out.println("You have drawn a " + game.player.getHand()[2].toString());
+
+      //BUSTING
+      if((game.player.getHand()[0].getValue() + game.player.getHand()[1].getValue()
+      + game.player.getHand()[2].getValue()) > 21) {
+          System.out.println(
+          + game.player.getHand()[0].getValue() + game.player.getHand()[1].getValue()
+          + game.player.getHand()[2].getValue() + " is your new hand Value, YOU BUST");
+          break;
+      }
+      //NOT BUSTING
+      if((game.player.getHand()[0].getValue() + game.player.getHand()[1].getValue()
+      + game.player.getHand()[2].getValue()) <= 21) {
+        System.out.println(game.player.getHand()[0].getValue() + game.player.getHand()[1].getValue()
+        + game.player.getHand()[2].getValue() + " is your new hand Value");
+      }
+
+    }
+
+//DEALER ACTIONS
+    System.out.println("The dealer shows " + game.dealer.getHand()[0].toString()
+    + " " + game.dealer.getHand()[1].toString());
+
    break;
   }
+  break;
  }
     }
 
