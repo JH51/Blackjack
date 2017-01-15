9934fbc33e13f997c361a9d4fe4b9d3149a278eb
JH51 <jonhuang5@gmail.com>
2017-01-15 02:36:31 -0500

    Added window on PLAY button press

--- BlackjackGUI.java
+++ BlackjackGUI.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.*;
+import java.awt.event.*;
 
 public class BlackjackGUI extends JFrame{
 
@@ -13,17 +13,28 @@ public class BlackjackGUI extends JFrame{
 	this.setSize(1280, 720);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+	
 	pane = this.getContentPane();
 	pane.setBackground( new Color(48,128,48) );
 	pane.setLayout(new FlowLayout(2,5,200));
-	JButton b1 = new JButton("Hit");
-	JButton b2 = new JButton("Stay");
-	JButton b3 = new JButton("Next Hand");
-	JDialog d1 = new JDialog(frame, "How much would you like to wager?");
+	JButton b1 = new JButton("PLAY");
+        b1.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+		    JDialog d = new JDialog(frame, "How much would you like to wager?", true);
+		    JTextField t1 = new JTextField();
+		    JButton b1_01 = new JButton("PLAY");
+		    d.add(t1);
+		    d.add(b1_01);
+		    d.setSize(640, 360);
+		    d.setLocationRelativeTo(frame);
+		    d.setVisible(true);
+		}
+	    }
+	    );
+	JButton b2 = new JButton("Hit");
+	JButton b3 = new JButton("Stay");
+	JButton b4 = new JButton("Next Hand");
 
-	d1.setLocationRelativeTo(pane);
-	d1.setVisible(true);
 	pane.add(b1);
 	pane.add(b2);
 	pane.add(b3);
